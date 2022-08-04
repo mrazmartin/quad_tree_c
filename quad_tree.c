7de0bd9c772f9a985b434a0a1b93cf482aefbb0a
mrazmartin <mraz.martin.cz@gmail.com>
2022-08-04 11:23:21 +0200

    file: quad_tree.c init

---
+++ quad_tree.c
@@ -0,0 +1,15 @@
+/*  
+    This work is inspired by a wikipedia article "https://en.wikipedia.org/wiki/Quadtree"
+    and a youtube tutorial by Aamir Mushtaq Siddiqui. However I made some modifications
+    to his code so it should be more robust and can be applied also for non-symmetrical 
+    cases.
+
+    Since I am only redicovering coding in C after a long break, the code is (and will) not
+    be optimal. Feel free to pull request via  "https://github.com/mrazmartin/quad_tree_c".
+
+    Outside of the mentioned ispiration tutorial is the code written by Martin Mráz.
+    https://github.com/mrazmartin
+
+    The code is should run cross-platform, it has been tested for Linux and Windows (with MinGW).
+*/
+
